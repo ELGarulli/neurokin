91684fe32d9d9c7b369887de1ee0c4b96afd7b57
ELGarulli <elisa.garulli@charite.de>
2022-09-27 17:20:07 +0200

    updated boundary detection to scipy based version

--- test_c3d_processing.py
+++ test_c3d_processing.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from utils.kinematics import kinematics_processing, angles
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -10,41 +12,32 @@ temp_df = pd.read_csv(filename_csv, sep="\t")
 
 sides = ["r", "l"]
 
-# for angle, markers in angles.angle_sides.items():
-#    fig, ax = plt.subplots()
-#    for s in sides:
-#        side_angle = []
-#        for frame in range(len(temp_df)):
-#            a, b, c = kinematics_processing.get_points(temp_df, markers, s, frame)
-#            side_angle.append(kinematics_processing.get_angle(a, b, c))
-#        ax.plot(side_angle, label=s)
-#    fig.suptitle(angle)
-#    plt.show()
-
-
 to_detrend = ["rshoulder_z", "rcrest_z", "rhip_z", "rknee_z", "rankle_z", "rmtp_z", "lshoulder_z", "lcrest_z", "lhip_z",
               "lknee_z", "lankle_z", "lmtp_z"]
+to_shift = ["rshoulder_y", "rcrest_y", "rhip_y", "rknee_y", "rankle_y", "rmtp_y", "lshoulder_y", "lcrest_y", "lhip_y",
+            "lknee_y", "lankle_y", "lmtp_y"]
 
-df_detrend = kinematics_processing.shift_correct(temp_df, "lmtp_z", to_detrend)
+df_detrend = kinematics_processing.tilt_correct(temp_df, "lmtp_z", to_detrend)
+df_detrend = kinematics_processing.shift_correct(df_detrend, "lmtp_y", to_shift)
+fig, axs = plt.subplots(1, 2)
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
 xs = temp_df["lmtp_x"]
 ys = temp_df["lmtp_y"]
 zs = temp_df["lmtp_z"]
-ax.scatter(xs, ys, zs)
+ys_ = temp_df["rhip_y"]
+zs_ = temp_df["rhip_z"]
+axs[0].scatter(ys, zs)
 
-xs_detrend = df_detrend["lmtp_x"]
+axs[1].scatter(ys_, zs_)
+
 ys_detrend = df_detrend["lmtp_y"]
 zs_detrend = df_detrend["lmtp_z"]
-ax.scatter(xs_detrend, ys_detrend, zs_detrend)
+ys_detrend_ = df_detrend["rhip_y"]
+zs_detrend_ = df_detrend["rhip_z"]
 
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
+axs[0].scatter(ys_detrend, zs_detrend)
+axs[1].scatter(ys_detrend_, zs_detrend_)
+axs[0].set_title("Left mtp marker")
+axs[1].set_title("Right hip marker")
 
-# ax.plot(temp_df["lmtp_x"], label="x")
-# ax.plot(temp_df["lmtp_y"], label="y")
-# ax.plot(temp_df["lmtp_z"], label="z")
-# plt.legend()
 plt.show()
