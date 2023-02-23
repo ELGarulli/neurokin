d7dae1aa082d522f0c812ec0c5e26272f6474bae
MSchellenberger <michael.sbg@gmx.de>
2023-02-23 19:14:43 +0100

    all DCL2kin features implemented except correlation; code reformatted with black

---
+++ neurokin/utils/Features/acceleration_dcl2kin.py
@@ -0,0 +1,32 @@
+import dlc2kinematics
+from typing import List, Dict, Any
+
+from .core import FeatureExtraction, DefaultParams
+
+
+class Feature_acceleration(FeatureExtraction):
+    """
+    Computes the acceleration of bodyparts
+    Input: df with positon data (i.e. DLC output), source_marker_ids: List of markers for which velocity should be computed
+    Output: df with acceleration data for input markers
+    """
+
+    @property
+    def default_values(self) -> Dict[str, Any]:
+        default_values = {"window_size": 3}
+        return default_values
+
+    @property
+    def default_value_types(self) -> Dict[str, List[type]]:
+        default_types = {"window_size": [int]}
+        return default_types
+
+    def _run_feature_extraction(self, source_marker_ids):
+
+        df_acceleration = dlc2kinematics.compute_acceleration(
+            df=self.marker_df,
+            bodyparts=source_marker_ids,
+            filter_window=self.params["window_size"],
+        )
+        self._assert_valid_output(output_df=df_acceleration)
+        return df_acceleration
---
+++ neurokin/utils/Features/angular_momentum_dcl2kin.py
@@ -0,0 +1,31 @@
+import dlc2kinematics
+from typing import List, Dict, Any
+
+from .core import FeatureExtraction, DefaultParams
+
+
+class Feature_angular_velocity(FeatureExtraction):
+    """
+    Computes the velocity of angles
+    Input: df with joint angle data, source_marker_ids: List of markers for which speed should be computed
+    Output: df with angular velocity for input markers
+    """
+
+    @property
+    def default_values(self) -> Dict[str, Any]:
+        default_values = {"window_size": 3}
+        return default_values
+
+    @property
+    def default_value_types(self) -> Dict[str, List[type]]:
+        default_types = {"window_size": [int]}
+        return default_types
+
+    def _run_feature_extraction(self, source_marker_ids):
+        df_angular_momentum = dlc2kinematics.compute_joint_velocity(
+            joint_angle=self.marker_df,
+            joints_dict=source_marker_ids,
+            filter_window=self.params["window_size"],
+        )
+        self._assert_valid_output(output_df=df_angular_momentum)
+        return df_angular_momentum
