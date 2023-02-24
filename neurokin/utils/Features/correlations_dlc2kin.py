27b380986fe77b56027361f615280c1e98268f9a
MSchellenberger <michael.sbg@gmx.de>
2023-02-24 10:12:03 +0100

    Restructured files, added correlation function

---
+++ neurokin/utils/Features/correlations_dlc2kin.py
@@ -0,0 +1,36 @@
+import dlc2kinematics
+from typing import List, Dict, Any
+
+from .core import FeatureExtraction, DefaultParams
+
+
+class Feature_angular_velocity(FeatureExtraction):
+    """
+    Computes the correlation of features
+    Input: markers_df, source_marker_ids: List of features to be correlated
+    Output: df with correlations for features
+    """
+
+    @property
+    def input_type(self):
+        return "correlations"
+
+    @property
+    def default_values(self) -> Dict[str, Any]:
+        default_values = {}
+        return default_values
+
+    @property
+    def default_value_types(self) -> Dict[str, List[type]]:
+        default_types = {}
+        return default_types
+
+    def _run_feature_extraction(self, source_marker_ids):
+
+        filtered_df = self._copy_filtered_columns_of_df(
+            df_to_filter=self.marker_df, marker_id_filter=source_marker_ids
+        )
+        df_correlation = dlc2kinematics.compute_correlation(filtered_df)
+
+        self._assert_valid_output(output_df=df_correlation)
+        return df_correlation
--- neurokin/utils/Features/joint_angles_dlc2kin.py
+++ neurokin/utils/Features/joint_angles_dlc2kin.py
@@ -13,7 +13,7 @@ class Feature_joint_angles(FeatureExtraction):
 
     @property
     def input_type(self):
-        return 'joints'
+        return "joints"
 
     @property
     def default_values(self) -> Dict[str, Any]:
@@ -34,3 +34,34 @@ class Feature_joint_angles(FeatureExtraction):
         )
         self._assert_valid_output(output_df=df_joint_angles)
         return df_joint_angles
+
+
+class Feature_angular_velocity(FeatureExtraction):
+    """
+    Computes the velocity of angles
+    Input: df with joint angle data, source_marker_ids: List of markers for which speed should be computed
+    Output: df with angular velocity for input markers
+    """
+
+    @property
+    def input_type(self):
+        return "joints"
+
+    @property
+    def default_values(self) -> Dict[str, Any]:
+        default_values = {"window_size": 3}
+        return default_values
+
+    @property
+    def default_value_types(self) -> Dict[str, List[type]]:
+        default_types = {"window_size": [int]}
+        return default_types
+
+    def _run_feature_extraction(self, source_marker_ids):
+        df_angular_momentum = dlc2kinematics.compute_joint_velocity(
+            joint_angle=self.marker_df,
+            joints_dict=source_marker_ids,
+            filter_window=self.params["window_size"],
+        )
+        self._assert_valid_output(output_df=df_angular_momentum)
+        return df_angular_momentum
