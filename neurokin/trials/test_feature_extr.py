5f95142f4c138f246a8a01d55b19f1c356d0b960
ELGarulli <elisa.garulli@charite.de>
2023-03-06 15:38:10 +0100

    working draft of binned feat extraction

--- neurokin/trials/test_feature_extr.py
+++ neurokin/trials/test_feature_extr.py
@@ -2,7 +2,6 @@ from neurokin.kinematic_data import KinematicDataRun
 from neurokin.neural_data import NeuralData
 from neurokin.utils.neural import processing, neural_plot
 
-
 GAIT_PATH = "./neurokin/test_data/"
 NEURAL_PATH = "./temp_data/neural/220915/ENWE_00052-220915-153059/"
 CONFIGPATH = "../../config.yaml"
@@ -23,23 +22,21 @@ to_shift = ["rshoulder_y", "rcrest_y", "rhip_y",
 step_left_marker = "lmtp"
 step_right_marker = "rmtp"
 
-
-
 file = "../test_data/runway03.c3d"
 
-
-
-
-kin_data = KinematicDataRun(file, CONFIGPATH)       # creating a single run obj
+kin_data = KinematicDataRun(file, CONFIGPATH)  # creating a single run obj
 kin_data.load_kinematics()
 
-kin_data.compute_gait_cycles_bounds(left_marker=step_left_marker,   # computing left right bounds of steps
+kin_data.compute_gait_cycles_bounds(left_marker=step_left_marker,  # computing left right bounds of steps
                                     right_marker=step_right_marker)
-kin_data.print_step_partition(step_left_marker, step_right_marker, output_folder)  # print step partition for inspection only
+kin_data.print_step_partition(step_left_marker, step_right_marker,
+                              output_folder)  # print step partition for inspection only
 
 kin_data.get_c3d_compliance()
 
-
+bodyparts_to_drop = [i[1] for i in kin_data.markers_df.columns.to_list()[::3] if i[1].startswith("*")]
+kin_data.markers_df = kin_data.markers_df.drop(bodyparts_to_drop, axis=1, level=1, inplace=False)
+kin_data.bodyparts =  [bp for bp in kin_data.bodyparts if bp not in bodyparts_to_drop]
 kin_data.extract_features()
 
 test = kin_data.get_binned_features()
--- neurokin/utils/Features/binning.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-def get_easy_metrics_on_bins(df_markers, df_features, window, overlap):
-    df_ = df_features.columns.drop(df_markers.columns.tolist())
-
-    means = df_.rolling(window=window, step=overlap).mean()
-    return means
