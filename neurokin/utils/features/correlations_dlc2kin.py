e687665e1449a01a2d7a0473c2a2097955a38488
MSchellenberger <michael.sbg@gmx.de>
2023-05-03 10:07:20 +0200

    First draft of correlations feature

--- neurokin/utils/features/correlations_dlc2kin.py
+++ neurokin/utils/features/correlations_dlc2kin.py
@@ -14,16 +14,16 @@ class CorrelationDLC(FeatureExtraction):
 
     @property
     def input_type(self):
-        return "multiple_markers"
+        return "correlations"
 
     @property
     def default_values(self) -> Dict[str, Any]:
-        default_values = {"features_to_correlate": None}
+        default_values = {}
         return default_values
 
     @property
     def default_value_types(self) -> Dict[str, List[type]]:
-        default_types = {"features_to_correlate": [str, ]}
+        default_types = {}
         return default_types
 
     def _run_feature_extraction(
@@ -36,9 +36,19 @@ class CorrelationDLC(FeatureExtraction):
         # filter df for specific columns, raise error if angles not calculated yet
         # params defining properties
         filtered_df = self._copy_filtered_columns_of_df(
-            df_to_filter=marker_df, marker_id_filter=[source_marker_ids]
+            df_to_filter=marker_df, marker_id_filter=source_marker_ids
         )
+
+
         df_correlation = dlc2kinematics.compute_correlation(filtered_df)
 
-        self._assert_valid_output(output_df=df_correlation, marker_df=marker_df)
-        return df_correlation
+        names = marker_df.columns.names
+        scorer = marker_df.columns.get_level_values("scorer")[0]
+        columns = ["angle" for i in range(len(df_correlation.columns.to_list()))]
+        df_correlation_reshaped = df_correlation.copy()
+        df_correlation_reshaped.columns = pd.MultiIndex.from_product(
+            [[scorer], source_marker_ids.keys(), columns],
+            names=names)
+
+        self._assert_valid_output(output_df=df_correlation_reshaped, marker_df=marker_df)
+        return df_correlation_reshaped
