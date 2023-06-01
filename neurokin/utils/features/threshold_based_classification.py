d6ab6180825fbeb83dbe83a6fb979f6f6c7570d8
MSchellenberger <michael.sbg@gmx.de>
2023-06-01 16:44:18 +0200

    Restructure of threshold based classification

--- neurokin/utils/features/threshold_based_classification.py
+++ neurokin/utils/features/threshold_based_classification.py
@@ -56,18 +56,16 @@ def _get_interval_border_idxs(
     return interval_border_idxs, df
 
 
-class ImmobilityAndFreezing(FeatureExtraction):
+class Immobility(FeatureExtraction):
     """
     Classifies bodyparts as immobile if the bodyparts speed is below a speed threshold
     Input:  df with positon data (i.e. DLC output) and speed for critical bodyparts
-            source_marker_ids: List of m should be computed
+            source_marker_ids: List of markers for which immobility should be classified
     Additional information required in config file:
         immobility_threshold: float, speed threshold for immobility
-        markers_for_immobility: markers used for immobility detection
         minimum_duration_immobility: float, minimum duration of immobility bouts
-        minimum_duration_freezing: float, minimum duration of freezing bouts
         fps: int, frames per second of recording
-    Output: df with immobility and freezing bouts for subject
+    Output: df with immobility bouts for each bodypart specified in source_marker_ids
     """
 
     @property
@@ -80,7 +78,6 @@ class ImmobilityAndFreezing(FeatureExtraction):
             "immobility_threshold": 3,
             "markers_for_immobility": ["Snout", "TailBase"],
             "minimum_duration_immobility": 0.1,
-            "minimum_duration_freezing": 0.5,
             "fps": 80,
         }
         return default_values
@@ -91,7 +88,6 @@ class ImmobilityAndFreezing(FeatureExtraction):
             "immobility_threshold": int,
             "markers_for_immobility": [str],
             "minimum_duration_immobility": int,
-            "minimum_duration_freezing": int,
             "fps": int,
         }
         return default_types
@@ -103,7 +99,7 @@ class ImmobilityAndFreezing(FeatureExtraction):
         params: Dict[str, Any],
     ) -> pd.DataFrame:
         # Check whether immobility is already calculated
-        if ("scorer", "subject", "immobility") in marker_df.columns:
+        if ("scorer", source_marker_ids, "immobility") in marker_df.columns:
             pass
         else:
             # Check whether speed is calculated for all markers critical for immobility
@@ -111,73 +107,35 @@ class ImmobilityAndFreezing(FeatureExtraction):
                 ("scorer", column, "speed") in marker_df.columns
                 for column in params["markers_for_immobility"]
             ):
-                valid_idxs_per_marker_id = []
-                for bodypart_id in params["markers_for_immobility"]:
-                    temp_df = self._copy_filtered_columns_of_df(
-                        df_to_filter=marker_df,
-                        marker_id_filter=bodypart_id,
-                        coords_filter=["speed"],
-                    )
-                    # remove multiindex to make it easier to work with
-                    temp_df = temp_df.droplevel([0, 1], axis=1)
 
-                    # create new col for immobility, set val to false
-                    temp_df["immobility"] = False
-                    # filter for rows in which the speed is less than the immobility threshold.
-                    temp_df.loc[
-                        (temp_df["speed"] < params["immobility_threshold"]),
-                        "immobility",
-                    ] = True
+                immobility_bout_idxs = []
 
-                    valid_idxs_per_marker_id.append(
-                        temp_df.loc[temp_df["immobility"] == True].index.values
-                    )
-                shared_valid_idxs_for_all_markers = valid_idxs_per_marker_id[0]
-                if len(valid_idxs_per_marker_id) > 1:
-                    for next_set_of_valid_idxs in valid_idxs_per_marker_id[1:]:
-                        shared_valid_idxs_for_all_markers = np.intersect1d(
-                            shared_valid_idxs_for_all_markers, next_set_of_valid_idxs
-                        )
-
-                # To Do: merging immobility bouts
-
-                # create col in df for immobility
-                immobility_and_freezing_df = self._copy_filtered_columns_of_df(
+                filtered_df = self._copy_filtered_columns_of_df(
                     df_to_filter=marker_df,
                     marker_id_filter=source_marker_ids,
                     coords_filter=["speed"],
                 )
-                immobility_and_freezing_df = immobility_and_freezing_df.droplevel(
-                    [0, 1], axis=1
-                )
+                # remove multiindex to make it easier to work with
+                filtered_df = filtered_df.droplevel([0, 1], axis=1)
 
-                immobility_and_freezing_df["immobility"] = False
+                # create new col for immobility, set val to false
+                filtered_df["immobility"] = False
+                # filter for rows in which the speed is less than the immobility threshold.
+                filtered_df.loc[
+                    (filtered_df["speed"] < params["immobility_threshold"]),
+                    "immobility",
+                ] = True
 
-                # calculate bout duration and create col in df for it
-                (
-                    immobility_interval_border_idxs,
-                    immobility_and_freezing_df,
+                immobility_bout_idxs.append(filtered_df.loc[filtered_df["immobility"] == True].index.values)
+
+                (immobility_interval_border_idxs,
+                    immobility_df,
                 ) = _get_interval_border_idxs(
-                    all_matching_idxs=shared_valid_idxs_for_all_markers,
+                    all_matching_idxs=immobility_bout_idxs,
                     min_interval_duration=params["minimum_duration_immobility"],
                     fps=params["fps"],
-                    df=immobility_and_freezing_df,
+                    df=filtered_df,
                     colname="immobility",
-                )
-
-                # create col in df for freezing
-                immobility_and_freezing_df["freezing"] = False
-
-                # calculate bout duration and create col in df for freezing
-                (
-                    freezing_interval_border_idxs,
-                    immobility_and_freezing_df,
-                ) = _get_interval_border_idxs(
-                    all_matching_idxs=shared_valid_idxs_for_all_markers,
-                    min_interval_duration=params["minimum_duration_freezing"],
-                    fps=params["fps"],
-                    df=immobility_and_freezing_df,
-                    colname="freezing",
                 )
 
                 # transform dfs into multiindex df
@@ -187,7 +145,7 @@ class ImmobilityAndFreezing(FeatureExtraction):
                     scorer="scorer",
                     bodypart="subject",
                     axis="immobility",
-                    data=immobility_and_freezing_df.loc[:, "immobility"],
+                    data=immobility_df.loc[:, "immobility"],
                 )
 
                 # immobility bout duration
@@ -195,55 +153,161 @@ class ImmobilityAndFreezing(FeatureExtraction):
                     scorer="scorer",
                     bodypart="subject",
                     axis="immobility_bout_duration",
-                    data=immobility_and_freezing_df.loc[:, "immobility_bout_duration"],
+                    data=immobility_df.loc[:, "immobility_bout_duration"],
                 )
 
-                # freezing column (boolean)
+                final_df = pd.concat([immobility_col_df, immobility_bout_df], axis=1)
+
+                return final_df
+
+class Freezing(FeatureExtraction):
+    """
+    Classifies subject as freezing if the necessary markers are immobile
+    Input:  df with positon data (i.e. DLC output) and immobility for critical bodyparts
+            source_marker_ids: List of markers, not used in the function but needs to get passed
+    Additional information required in config file:
+        markers_for_freezing: markers used for immobility detection
+        minimum_duration_freezing: float, minimum duration of freezing bouts
+        fps: int, frames per second of recording
+    Output: df with freezing bouts for subject
+    """
+
+    @property
+    def input_type(self) -> str:
+        return "markers"
+
+    @property
+    def default_values(self) -> Dict[str, Any]:
+        default_values = {
+            "markers_for_freezing": ["Snout", "TailBase"],
+            "front_marker": "Snout",
+            "minimum_duration_freezing": 0.5,
+            "fps": 80,
+        }
+        return default_values
+
+    @property
+    def default_value_types(self) -> Dict[str, List[type]]:
+        default_types = {
+            "markers_for_freezing": [str],
+            "front_marker": str,
+            "minimum_duration_freezing": int,
+            "fps": int,
+        }
+        return default_types
+
+    def _run_feature_extraction(
+            self,
+            source_marker_ids: List[str],
+            marker_df: pd.DataFrame,
+            params: Dict[str, Any],
+    ) -> pd.DataFrame:
+
+        # Check whether freezing is already calculated
+        if ("scorer", "subject", "freezing") in marker_df.columns:
+            pass
+        else:
+            # Check whether speed is calculated for all markers critical for immobility
+            if all(
+                    ("scorer", column, "immobility") in marker_df.columns
+                    for column in params["markers_for_freezing"]
+            ):
+
+                filtered_df = self._copy_filtered_columns_of_df(
+                    df_to_filter=marker_df,
+                    marker_id_filter=[params["markers_for_freezing"]],
+                    coords_filter=["immobility", "immobility_bout_duration"],
+                )
+                # drop scorer level
+                filtered_df = filtered_df.droplevel([0], axis=1)
+
+                # get idxs of freezing bouts -> idxs of where all markers are immobile
+                # and immobility bout duration is >= minimum_duration_freezing
+                freezing_bout_idxs_per_marker = np.where(all(filtered_df[marker]["immobility"] == True
+                                                         for marker in params["markers_for_freezing"])&
+                                                         all(filtered_df[marker]["immobility_bout_duration"]
+                                                         >= params["minimum_duration_freezing"]
+                                                         for marker in params["markers_for_freezing"]))
+                # drop bodypart level
+                filtered_df.droplevel([0], axis=1)
+
+                # get freezing bout starts and ends
+                freezing_bout_start_idxs = np.where((elem in freezing_bout_idxs_per_marker)
+                                    and (elem - 1 not in freezing_bout_idxs_per_marker)
+                                    for elem in freezing_bout_idxs_per_marker)
+
+                freezing_bout_end_idxs = np.where((elem in freezing_bout_idxs_per_marker)
+                                      and (elem+1 not in freezing_bout_idxs_per_marker)
+                                      for elem in freezing_bout_idxs_per_marker)
+
+                # create new col for freezing, set val to false
+                filtered_df["freezing"] = False
+
+                # get x position of front marker -> place of freezing
+                x_position_df = self._copy_filtered_columns_of_df(
+                    df_to_filter=marker_df,
+                    marker_id_filter=[params["front_marker"]],
+                    coords_filter=["x"],
+                )
+                x_position_df = x_position_df.droplevel([0, 1], axis=1)
+
+                # iterate through idxs
+                # set gait disruption to true, determine duration, x position, and bout nr
+                bout_nr = 1
+                for start_idx, end_idx in zip(
+                        freezing_bout_start_idxs, freezing_bout_end_idxs
+                ):
+
+                    filtered_df.loc[start_idx:end_idx, "freezing"] = True
+                    interval_duration = filtered_df.loc[
+                        start_idx, "immobility_bout_duration"
+                    ]
+                    filtered_df.loc[
+                    start_idx:end_idx, "freezing_bout_duration"
+                    ] = interval_duration
+                    x_position = x_position_df.loc[start_idx, 'x']
+                    filtered_df.loc[start_idx:end_idx, "freezing_x_position"] = x_position
+                    filtered_df.loc[start_idx:end_idx, "freezing_bout_nr"] = bout_nr
+                    bout_nr += 1
+
+                # transform dfs into multiindex df
+                # freezing
                 freezing_col_df = self.convert_singleindex_to_multiindex_df(
                     scorer="scorer",
                     bodypart="subject",
                     axis="freezing",
-                    data=immobility_and_freezing_df.loc[:, "freezing"],
+                    data=filtered_df.loc[:, "freezing"],
                 )
-                if "freezing_bout_duration" in immobility_and_freezing_df.columns:
-                    # freezing bout duration
-                    freezing_bout_df = self.convert_singleindex_to_multiindex_df(
-                        scorer="scorer",
-                        bodypart="subject",
-                        axis="freezing_bout_duration",
-                        data=immobility_and_freezing_df.loc[
-                            :, "freezing_bout_duration"
-                        ],
-                    )
-                if "freezing_bout_duration" not in immobility_and_freezing_df.columns:
-                    print("No freezing bouts detected")
-                    immobility_and_freezing_df["freezing_bout_duration"] = np.nan
-
-                    # freezing bout duration
-                freezing_bout_df = self.convert_singleindex_to_multiindex_df(
+                freezing_bout_duration_df = self.convert_singleindex_to_multiindex_df(
                     scorer="scorer",
                     bodypart="subject",
                     axis="freezing_bout_duration",
-                    data=immobility_and_freezing_df.loc[:, "freezing_bout_duration"],
+                    data=filtered_df.loc[:, "freezing_bout_duration"],
                 )
+                freezing_bout_x_position_df = self.convert_singleindex_to_multiindex_df(
+                    scorer="scorer",
+                    bodypart="subject",
+                    axis="freezing_bout_x_position",
+                    data=filtered_df.loc[:, "freezing_bout_x_position"],
+                )
+                freezing_bout_nr_df = self.convert_singleindex_to_multiindex_df(
+                    scorer="scorer",
+                    bodypart="subject",
+                    axis="freezing_bout_nr",
+                    data=filtered_df.loc[:, "freezing_bout_nr"],
+                )
+                final_df = pd.concat(
+                    [freezing_col_df,
+                     freezing_bout_duration_df,
+                     freezing_bout_x_position_df,
+                     freezing_bout_nr_df], axis=1
+                )
+                return final_df
 
-                final_immobility_df = pd.concat(
-                    [
-                        immobility_col_df,
-                        immobility_bout_df,
-                        freezing_col_df,
-                        freezing_bout_df,
-                    ],
-                    axis=1,
-                )
-                return final_immobility_df
-            #
-            #
             else:
                 raise ValueError(
-                    "Be sure to extract speed for all bodyparts that you want to use for immobility!"
+                    "Be sure to classify immobility for each bodypart before classifying freezing!"
                 )
-
 
 class GaitDisruption(FeatureExtraction):
     """
@@ -267,6 +331,7 @@ class GaitDisruption(FeatureExtraction):
     @property
     def default_values(self) -> Dict[str, Any]:
         default_values = {
+            "markers_for_gait_disruption": ["TailBase"],
             "min_duration_movement_before": 0.5,
             "min_duration_immobility": 0.1,
             "front_of_subject": "Snout",
@@ -277,6 +342,7 @@ class GaitDisruption(FeatureExtraction):
     @property
     def default_value_types(self) -> Dict[str, List[type]]:
         default_types = {
+            "markers_for_gait_disruption": List[str],
             "min_duration_movement_before": float,
             "min_duration_immobility": float,
             "front_of_subject": str,
@@ -296,7 +362,10 @@ class GaitDisruption(FeatureExtraction):
             pass
         else:
             # check whether immobility is calculated (necessary for gait disruption classification)
-            if ("scorer", "subject", "immobility") in marker_df.columns:
+            if all(
+                ("scorer", column, "immobility") in marker_df.columns
+                for column in params["markers_for_gait_disruption"]
+            ):
                 # slice marker df
                 filtered_df = self._copy_filtered_columns_of_df(
                     df_to_filter=marker_df,
