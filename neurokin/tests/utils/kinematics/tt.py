818c0e9d43d99fef65c08abd9444dc19f1dffccd
ELGarulli <elisa.garulli@charite.de>
2024-01-26 18:19:26 +0100

    progress in cleaning the state analysis correlates

--- neurokin/tests/utils/kinematics/tt.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pytest
-from neurokin.utils.kinematics import gait_params_basics
-
-
-print(gait_params_basics.get_phase_at_max_amplitude(np.sin(np.radians(np.arange(360)))))
