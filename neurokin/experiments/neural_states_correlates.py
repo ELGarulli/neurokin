45f6a63f3b26e9d6e57bfad342850930b1cb53b1
ELGarulli <elisa.garulli@charite.de>
2024-01-29 18:06:29 +0100

    wip to port all the aux functions to a clean version

--- neurokin/experiments/neural_states_correlates.py
+++ neurokin/experiments/neural_states_correlates.py
@@ -78,42 +78,43 @@ class NeuralCorrelatesStates():
         :param filename: filename without extension
         :return:
         """
-        accepted_datasets = ["events_dataset", "raw_neural_correlate_dataset", "psd_neural_correlate_dataset"]
+        accepted_datasets = ["events_dataset", "raw_neural_correlates_dataset", "psd_neural_correlates_dataset"]
         if dataset not in accepted_datasets:
             raise ValueError(f"Dataset not found. Please select one of the following : {accepted_datasets}")
 
         if dataset == "events_dataset":
             self._save_dataset(self.events_dataset_dict, filename)
         if dataset == "raw_neural_correlates_dataset":
+            self._save_dataset(self.fs, "fs")
             self._save_dataset(self.raw_neural_correlates_dict, filename)
         if dataset == "psd_neural_correlates_dataset":
             self._save_dataset(self.psds_correlates_dict, filename)
 
-    def create_events_dataset(self, experiment_path, conditions):
-        _conditions = conditions
-        all_events_dict = {_condition: {} for _condition in conditions}
+    def create_events_dataset(self, experiment_path, conditions_of_interest):
+        all_events_dict = {c: {} for c in conditions_of_interest}
         dates = [str(date) for date in self.experiment_structure.keys()]
         for date in dates:
-            all_events_dict.setdefault(date, {})
             animals = [animal for animal in self.experiment_structure[int(date)].keys()]
             for animal in animals:
-                all_events_dict[date].setdefault(animal, {})
                 conditions = [condition for condition in self.experiment_structure[int(date)][animal].keys()]
                 for condition in conditions:
-                    runs = [str(run) for run in self.experiment_structure[int(date)][animal][condition]]
-                    runs = ["0" + run if len(run) == 1 else run for run in runs]
-                    for run in runs:
-                        run_path = "/".join([experiment_path, date, animal, run]) + "/"
+                    if condition in conditions_of_interest:
+                        all_events_dict[condition].setdefault(date, {})
+                        all_events_dict[condition][date].setdefault(animal, {})
+                        runs = [str(run) for run in self.experiment_structure[int(date)][animal][condition]]
+                        runs = ["0" + run if len(run) == 1 else run for run in runs]
+                        for run in runs:
+                            run_path = "/".join([experiment_path, date, animal, run]) + "/"
 
-                        try:
-                            event_path = [run_path + fname for fname in os.listdir(run_path) if
-                                          re.match(r"[a-z_-]+[0-9]{2}.csv", fname)][0]
-                            events_dict = get_events_dict(event_path=event_path,
-                                                          skiprows=self.skiprows,
-                                                          framerate=self.framerate)
-                            all_events_dict[date][animal][run] = events_dict
-                        except FileNotFoundError as error:
-                            print(f"No .csv events File Found For {date}, {animal}, {run}")
+                            try:
+                                event_path = [run_path + fname for fname in os.listdir(run_path) if
+                                              re.match(r"(?i)[a-z_-]+[0-9]{2}.csv", fname)][0]
+                                events_dict = get_events_dict(event_path=event_path,
+                                                              skiprows=self.skiprows,
+                                                              framerate=self.framerate)
+                                all_events_dict[condition][date][animal][run] = events_dict
+                            except FileNotFoundError as error:
+                                print(f"No .csv events File Found For {date}, {animal}, {run}")
 
         self.events_dataset_dict = all_events_dict
 
@@ -124,22 +125,21 @@ class NeuralCorrelatesStates():
                   "If the dataset is loaded it should be assigned to the attribute events_dataset_dict")
 
             return
-        dataset_raw_correlates = {}
-        dates = [str(date) for date in self.experiment_structure.keys()]
+        conditions_of_interest = list(self.events_dataset_dict.keys())
+        dataset_raw_correlates = {c: {} for c in conditions_of_interest}
 
-        for date in dates:
-            dataset_raw_correlates.setdefault(date, {})
-            animals = [animal for animal in self.experiment_structure[int(date)].keys()]
-            for animal in animals:
-                dataset_raw_correlates[date].setdefault(animal, {})
-                conditions = [condition for condition in self.experiment_structure[int(date)][animal].keys()]
-                for condition in conditions:
-                    runs = [str(run) for run in self.experiment_structure[int(date)][animal][condition]]
-                    runs = ["0" + run if len(run) == 1 else run for run in runs]
+        for condition in conditions_of_interest:
+            dates = list(self.events_dataset_dict[condition].keys())
+            for date in dates:
+                animals = list(self.events_dataset_dict[condition][date].keys())
+                for animal in animals:
+                    runs = list(self.events_dataset_dict[condition][date][animal])
                     for run in runs:
+                        dataset_raw_correlates[condition].setdefault(date, {})
+                        dataset_raw_correlates[condition][date].setdefault(animal, {})
                         channel_of_interest = self.ch_of_interest[animal]
                         run_path = "/".join([experiment_path, date, animal, run]) + "/"
-                        event_dict = self.events_dataset_dict[date][animal][run]
+                        event_dict = self.events_dataset_dict[condition][date][animal][run]
 
                         raw_neural_correlate, fs = get_neural_correlates_dict(neural_path=run_path,
                                                                               channel_of_interest=channel_of_interest,
@@ -149,7 +149,7 @@ class NeuralCorrelatesStates():
                         if fs is not None:
                             self.fs = fs
 
-                        dataset_raw_correlates[date][animal][run] = raw_neural_correlate
+                        dataset_raw_correlates[condition][date][animal][run] = raw_neural_correlate
 
         self.raw_neural_correlates_dict = dataset_raw_correlates
 
@@ -160,22 +160,22 @@ class NeuralCorrelatesStates():
                   "If the dataset is loaded it should be assigned to the attribute raw_neural_correlates_dict")
             return
 
-        dataset_psd = {}
-        dates = [str(date) for date in self.experiment_structure.keys()]
+        conditions_of_interest = list(self.raw_neural_correlates_dict.keys())
+        dataset_psd = {c: {} for c in conditions_of_interest}
+
         self.freqs = None
 
-        for date in dates:
-            dataset_psd.setdefault(date, {})
-            animals = [animal for animal in self.experiment_structure[int(date)].keys()]
-            for animal in animals:
-                dataset_psd[date].setdefault(animal, {})
-                conditions = [condition for condition in self.experiment_structure[int(date)][animal].keys()]
-                for condition in conditions:
-                    runs = [str(run) for run in self.experiment_structure[int(date)][animal][condition]]
-                    runs = ["0" + run if len(run) == 1 else run for run in runs]
+        for condition in conditions_of_interest:
+            dates = list(self.raw_neural_correlates_dict[condition].keys())
+            for date in dates:
+                animals = list(self.raw_neural_correlates_dict[condition][date].keys())
+                for animal in animals:
+                    runs = list(self.raw_neural_correlates_dict[condition][date][animal])
                     for run in runs:
-                        neural_dict = self.raw_neural_correlates_dict[date][animal][run]
-                        dataset_psd[date][animal][run], freqs_ = get_psd_dict(neural_dict=neural_dict,
+                        dataset_psd[condition].setdefault(date, {})
+                        dataset_psd[condition][date].setdefault(animal, {})
+                        neural_dict = self.raw_neural_correlates_dict[condition][date][animal][run]
+                        dataset_psd[condition][date][animal][run], freqs_ = get_psd_dict(neural_dict=neural_dict,
                                                                               fs=self.fs,
                                                                               nfft=nfft,
                                                                               noverlap=nov,
