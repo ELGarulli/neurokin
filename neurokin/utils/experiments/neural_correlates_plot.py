d01815a2a2e149f7ff04e08ea830be6e42437589
ELGarulli <elisa.garulli@charite.de>
2024-05-24 17:08:54 +0200

    almost got the plotting. Check the FIXME

--- neurokin/utils/experiments/neural_correlates_plot.py
+++ neurokin/utils/experiments/neural_correlates_plot.py
@@ -1,8 +1,57 @@
 import numpy as np
 from scipy import stats
+from neurokin.utils.experiments.neural_states_helper import compute_ci
+
+def plot_psd_single_state(ax, df,  group, condition, state, freqs, color, idx_min, idx_max, stat="std", linewidth=3):
+    """
+
+    :param ax:
+    :param group:
+    :param condition:
+    :param state:
+    :param freqs:
+    :param idx_min:
+    :param idx_max:
+    :param color:
+    :param stat:
+    :param linewidth:
+    :return:
+    """
+
+    f = freqs[idx_min:idx_max]
+    data = df[(df["group"] == group) & (df["condition"]==condition)][state]
+    data_mean = data.mean(axis=0)[idx_min:idx_max]
+    ax.plot(f, data_mean, color=color, linewidth=linewidth)
+    add_shades(ax, f, data, idx_min, idx_max, color, stat=stat)
 
 
-def plot_psd_single_state(ax, group_cond_dataset, freqs, idx_min, idx_max, color, stat="std", linewidth=3):
+def add_shades(ax, f, data, idx_min, idx_max, color, stat="std"):
+    """
+    Helper function to plot shades.
+    :param ax: the ax to plot on
+    :param f: frequencies to use on the x-axis
+    :param data: the data to be plot (array like of states)
+    :param idx_min: minimum frequency index to plot
+    :param idx_max: maximum frequency index to plot
+    :param color: color to plot the shade in, with 0.3 alpha factor
+    :param stat: which statistics to use for shades. "std" for standard deviation,
+                "sem" for standard error of mean, "ci" for confidence interval
+    :return:
+    """
+    if stat == "sem":
+        s = data.sem(axis=0)
+    elif stat == "ci":
+        s = data.apply(compute_ci, axis=0)
+    elif stat == "std":
+        s = data.std(axis=0) #FIXME error possibly because it's cast to series. Check if the std is on the right axis
+    else:
+        raise ValueError(f"The statistic value {stat} is unsupported, please choose between std, sem or ci")
+
+    lower = data.mean(axis=0) - s
+    upper = data.mean(axis=0) + s
+    ax.fill_between(f, lower[idx_min:idx_max], upper[idx_min:idx_max], alpha=0.3, color=color)
+
+def _plot_psd_single_state(ax, group_cond_dataset, freqs, idx_min, idx_max, color, stat="std", linewidth=3):
     """
     Helper function to plot the mean of psds and the respective shade, indicating a statistical interval
     :param ax: the ax to plot on
@@ -23,7 +72,7 @@ def plot_psd_single_state(ax, group_cond_dataset, freqs, idx_min, idx_max, color
     add_shades(ax, f, group_cond_dataset, idx_min, idx_max, color, stat=stat)
 
 
-def add_shades(ax, f, data, idx_min, idx_max, color, stat="std"):
+def _add_shades(ax, f, data, idx_min, idx_max, color, stat="std"):
     """
     Helper function to plot shades.
     :param ax: the ax to plot on
