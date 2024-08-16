2e0b79f9eb1de5bbca08c1d9735bbabed64e113b
ELGarulli <85746126+ELGarulli@users.noreply.github.com>
2024-08-16 15:47:24 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['spinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
+extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
