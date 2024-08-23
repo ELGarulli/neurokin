cc80f30998c9adbb87d31f83af2908142503c6a6
ELGarulli <elisa.garulli@charite.de>
2024-08-23 14:47:35 +0200

    refactored type checks on params and extraction target thanks @niklasmueboe

--- neurokin/utils/features/core_elg.py
+++ neurokin/utils/features/core_elg.py
@@ -1,62 +1,31 @@
 from abc import ABC, abstractmethod
-from typing import List, get_type_hints, get_args, get_origin, final
+from typing import List, Union
+
+from typeguard import typechecked
 
 
 class FeatureExtraction(ABC):
 
-    def __init__(self, extraction_target_value):
-        self.extraction_target = extraction_target_value
-
-    @property
-    def extraction_target(self):
-        return self._extraction_target
-
-    @extraction_target.setter
-    def extraction_target(self, extraction_target_value):
-        if isinstance(extraction_target_value, str):
-            self._extraction_target = extraction_target_value
-        else:
-            raise TypeError(f"Extraction target type not valid, expected str got {type(extraction_target_value)}")
-
-    def check_expected_input_types(self):
-        type_hints = get_type_hints(self.__init__)
-        param_values = self.__dict__
-
-        for key, expected_type in type_hints.items():
-            origin = get_origin(expected_type)
-            args = get_args(expected_type)
-            actual_value = param_values[key]
-
-            if origin:
-                if not isinstance(actual_value, origin):
-                    raise TypeError(f"Parameter {key} is of incorrect type. "
-                                    f"Expected {origin} got {type(actual_value)}")
-                if args:
-                    for i, item in enumerate(actual_value):
-                        if not isinstance(item, args[0]):
-                            raise TypeError(f"Argument #{i} of parameter {key} is of incorrect type. "
-                                            f"Expected {args[0]} got {type(item)}")
-            else:
-                if not isinstance(actual_value, expected_type):
-                    raise TypeError(f"Parameter {key} is of incorrect type. "
-                                    f"Expected {expected_type} got {type(actual_value)}")
-
+    def __new__(cls, *args, **kwargs):
+        instance = super().__new__(cls)
+        if not isinstance(cls.extraction_target, str):
+            raise TypeError(f"Extraction target type not valid, expected str got {type(cls.extraction_target)}")
+        return instance
 
     @abstractmethod
     def compute_feature(self):
         pass
 
     def run_feat_extraction(self):
-        self.check_expected_input_types()
         self.compute_feature()
         return
 
 
-
 class MyFeat(FeatureExtraction):
+    extraction_target = "markers"
 
-    def __init__(self, param_a: float, param_b: List[int]):
-        super().__init__(extraction_target_value="markers")
+    @typechecked
+    def __init__(self, param_a: List[Union[int, str]], param_b: List[int]):
         self.param_a = param_a
         self.param_b = param_b
 
@@ -65,12 +34,10 @@ class MyFeat(FeatureExtraction):
         return
 
 
-
-
 class MyFeat2(FeatureExtraction):
+    extraction_target = "joints"
 
-    def __init__(self, param_a: float, param_b: List[int]):
-        super().__init__(extraction_target_value="joints")
+    def __init__(self, param_a: ABC, param_b: List[int]):
         self.param_a = param_a
         self.param_b = param_b
 
@@ -79,11 +46,11 @@ class MyFeat2(FeatureExtraction):
 
 
 if __name__ == "__main__":
-    feat = MyFeat(param_a=3.0, param_b=[1, 2, 3])
+    feat = MyFeat(param_a=[1, "str"], param_b=[1, 2, 3])
     feat2 = MyFeat2(param_a=3.0, param_b=[1, 2, 3])
-    feat2.run_feat_extraction()
+    # feat2.run_feat_extraction()
     feat.run_feat_extraction()
     print(feat.extraction_target)
-    feat.extraction_target = "joints"
+    # feat.extraction_target = "joints"
     print(feat2.extraction_target)
-    print(feat.extraction_target)
+    # print(feat.extraction_target)
